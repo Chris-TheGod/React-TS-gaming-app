3e8b227a4df168a563652864644bac418d562e1c
Kristjan Lääne <xonproject@gmail.com>
2024-05-07 19:17:36 +0300

    listgroup

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,25 @@
+import { useState } from 'react';
+
 export const ListGroup = () => {
-  const items = ['New York', 'San Fransisco', 'Tokyo', 'London', 'Paris'];
+  let items = ['New York', 'San Fransisco', 'Tokyo', 'London', 'Paris'];
+
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No items found...</p>}
       <ul className='list-group'>
         {items.map((item, index) => (
-          <li className='list-group-item' key={index}>
+          <li
+            className={
+              selectedIndex === index
+                ? 'list-group-item active'
+                : 'list-group-item'
+            }
+            key={index}
+            onClick={() => setSelectedIndex(index)}
+          >
             {item}
           </li>
         ))}
