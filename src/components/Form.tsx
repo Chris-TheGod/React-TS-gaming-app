16541fe69756d6d5f417e59c7c2d25daa9e3b6c3
Kristjan Lääne <xonproject@gmail.com>
2024-05-13 18:08:17 +0300

    form2

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,13 @@
+import { FormEvent } from 'react';
+
 export const Form = () => {
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log('submitted!');
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className='mb-3'>
         <label htmlFor='name' className='form-label'>
           Name
